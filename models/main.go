d2e77bb712eb735706b55934f7fd2ddd31e95131
apaudel96 <apaudel96@ymail.com>
2023-04-04 02:00:35 +0100

    dockerize app

--- models/main.go
+++ models/main.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"io/fs"
+	"os"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -8,6 +11,8 @@ import (
 var Db *gorm.DB
 
 func init() {
+	// create data folder if it doesn't exist
+	os.Mkdir("data", fs.ModePerm)
 	db, err := gorm.Open(sqlite.Open("data/pastebin.db"))
 	if err != nil {
 		panic(err)
