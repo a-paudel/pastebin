7ca5ba27e8278821444fb8396235152159355d2d
a-paudel <ankur@paudel.me>
2023-07-18 13:24:45 +0100

    remove node modules from git

--- node_modules/.pnpm/@mrleebo+prisma-ast@0.6.0/node_modules/@mrleebo/prisma-ast/dist/printSchema.d.ts
+++
@@ -1,7 +0,0 @@
-import * as Types from './getSchema';
-export interface PrintOptions {
-    sort?: boolean;
-    locales?: string | string[];
-    sortOrder?: Array<'generator' | 'datasource' | 'model' | 'view' | 'enum'>;
-}
-export declare function printSchema(schema: Types.Schema, options?: PrintOptions): string;
--- node_modules/.pnpm/prisma-kysely@1.5.0/node_modules/prisma-kysely/dist/utils/isValidTSIdentifier.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isValidTSIdentifier = (ident) => !!ident && /^[a-zA-Z_$][a-zA-Z_$0-9]*$/.test(ident);
-exports.default = isValidTSIdentifier;
-//# sourceMappingURL=isValidTSIdentifier.js.map
