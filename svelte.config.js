0786001042409f6377514887fd5de7c01d80706e
a-paudel <ankur@paudel.me>
2023-07-20 13:25:17 +0100

    move to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from "@sveltejs/adapter-node";
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-vercel";
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
@@ -14,10 +14,7 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      routes: {
-        include: ["/*"],
-        exclude: ["<all>"],
-      },
+      runtime: "edge",
     }),
   },
 };
